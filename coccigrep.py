e4c7165ed5fc7351399fd04beff875168b4d2c1b
Eric Leblond <eric@regit.org>
2011-08-13 21:53:03 +0200

    Major update
    This patch adds:
     * Add GPLv3 licence
     * Add used option
     * Fix temporary file suppression

--- coccigrep.py
+++ coccigrep.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python
+# Copyright (C) 2011 Eric Leblond <eric@regit.org>
+#
+# You can copy, redistribute or modify this Program under the terms of
+# the GNU General Public License version 3 as published by the Free
+# Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# version 3 along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 
 from subprocess import call, Popen, PIPE
 from tempfile import NamedTemporaryFile
 from os import unlink
 import argparse
 from sys import exit
+
+SPATCH="spatch"
+
+cocci_grep_struct_attribute_used="""@init@
+typedef %s;
+%s *p;
+position p1;
+@@
+
+p->%s@p1
+"""
 
 cocci_grep_struct_attribute_set="""@init@
 typedef %s;
@@ -18,10 +45,6 @@ p->%s@p1 |= E
 |
 p->%s@p1 = E
 )
-
-@ script:python @
-p1 << init.p1;
-@@
 """
 
 cocci_grep_struct_attribute_test="""@init@
@@ -38,13 +61,14 @@ p->%s@p1 != E
 |
 p->%s@p1 & E
 )
+"""
+
+cocci_python="""
 
 @ script:python @
 p1 << init.p1;
 @@
-"""
 
-cocci_python="""
 for p in p1:
     print "%s:%s:" % (p.file,p.line)
     f = open(p.file, 'r')
@@ -58,7 +82,7 @@ for p in p1:
 parser = argparse.ArgumentParser(description='Semantic grep based on coccinelle')
 parser.add_argument('-t', '--type', default='Signature', help='C type where looking for')
 parser.add_argument('-a', '--attribut', default='flags', help='C attribut that is set')
-parser.add_argument('-o', '--operation', default='set', help='Operation on structure (set, test)')
+parser.add_argument('-o', '--operation', default='used', help='Operation on structure (set, test)')
 parser.add_argument('file', metavar='file', nargs='+', help='List of files')
 args = parser.parse_args()
 
@@ -71,18 +95,21 @@ if args.operation == 'set':
     cocci_grep = cocci_grep_struct_attribute_set % (args.type, args.type, args.attribut, args.attribut) + cocci_python
 elif args.operation == 'test':
     cocci_grep = cocci_grep_struct_attribute_test % (args.type, args.type, args.attribut, args.attribut, args.attribut) + cocci_python
+elif args.operation == 'used':
+    cocci_grep = cocci_grep_struct_attribute_used % (args.type, args.type, args.attribut) + cocci_python
 else:
     print "unkown method"
     exit(1)
 
 tmp_cocci_file.write(cocci_grep)
+tmp_cocci_file.close()
 
 # launch spatch
-cmd = ["spatch", "-sp_file", tmp_cocci_file.name] + args.file
+cmd = [SPATCH, "-sp_file", tmp_cocci_file.name] + args.file
 print "running: %s." % " ".join(cmd)
 
-output = Popen(cmd)
+
+output = call(cmd)
 
 #print output
-tmp_cocci_file.close()
-#unlink(tmp_cocci_file_name)
+unlink(tmp_cocci_file_name)
