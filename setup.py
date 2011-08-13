2c7c28d26f20eeb5a9664131200a6f06dbca4e1d
Eric Leblond <eric@regit.org>
2011-08-13 23:34:26 +0200

    Basic setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='coccigrep',
+      version='0.1',
+      description='Semantic grep for C based on coccinelle',
+      author='Eric Leblond',
+      author_email='eric@regit.org',
+      url='http://home.regit.org/software/coccigrep/',
+      scripts=['coccigrep'],
+      )
