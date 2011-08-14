b59afc1fe17eacf9839d0add586d47b5e009ab25
Eric Leblond <eric@regit.org>
2011-08-14 18:06:57 +0200

    Set version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='coccigrep',
-      version='0.3',
+      version='0.4',
       description='Semantic grep for C based on coccinelle',
       author='Eric Leblond',
       author_email='eric@regit.org',
