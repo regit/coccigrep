c5cead383159b04c3cb267d3d9d2c765cddd1a7a
Eric Leblond <eric@regit.org>
2011-08-27 16:50:59 +0200

    Add basic cocci-grep emacs module
    When loaded into emacs, this module provides a cocci-grep function
    which does a call to coccigrep and provide an output in grep-mode.

---
+++ editors/cocci-grep.el
@@ -0,0 +1,87 @@
+;; Copyright (C) 2011 Eric Leblond
+;;
+;; this file is not part of Emacs
+;;
+;; Author: Eric Leblond
+;; Maintainer: Eric Leblond
+;; Description: provide interface to coccigrep
+;;
+;; Based on xargs-grep module by Le Wang
+;;
+;;; Installation: put this file in your load-path and
+;;
+;; (require 'cocci-grep)
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'compile)
+(require 'grep)
+
+(defvar cocci-s-type-history '()
+  "The minibuffer history list for `\\[cocci-grep]'s type argument.")
+
+(defvar cocci-s-attribut-history '()
+  "The minibuffer history list for `\\[cocci-grep]'s attribut argument.")
+
+(defvar cocci-s-operation-history '()
+  "The minibuffer history list for `\\[cocci-grep]'s operation argument.")
+
+(defvar cocci-files-history '()
+  "The minibuffer history list for `\\[cocci-grep]'s files argument.")
+
+
+(defun cocci-grep-read-type ()
+  (read-from-minibuffer "Type: "
+                        nil nil nil
+                        'cocci-s-type-history
+                        nil))
+(defun cocci-grep-read-attribut ()
+  (read-from-minibuffer "Attribut: "
+                        nil nil nil
+                        'cocci-s-attribut-history
+                        nil))
+(defun cocci-grep-read-operation ()
+  (read-from-minibuffer "Operation: "
+                        nil nil nil
+                        'cocci-s-attribut-operation
+                        nil))
+(defun cocci-grep-read-file-string ()
+  (read-from-minibuffer "Files: "
+   nil nil nil
+   'cocci-files-history
+   nil))
+
+;;;###autoload
+(defun cocci-grep (s-type s-attribut s-operation files)
+  "GREP-REGEXP is the regular expression to be used.
+FILES is a list of files to grep through."
+  (interactive (list
+                (cocci-grep-read-type)
+                (cocci-grep-read-attribut)
+                (cocci-grep-read-operation)
+                (cocci-grep-read-file-string)))
+  (let (out-buf
+        )
+    (setq out-buf (compilation-start (concat "coccigrep -E -t " s-type " -a " s-attribut
+                                             " -o " s-operation " " files) 'grep-mode))
+    ))
+
+(provide 'cocci-grep)
+
