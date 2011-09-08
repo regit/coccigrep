aa66b6bde935a61894d0f22070047f87b192e2ea
Eric Leblond <eric@regit.org>
2011-09-08 19:17:55 +0200

    Set release number to 1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='coccigrep',
-      version='1.0rc2',
+      version='1.0',
       description='Semantic grep for C based on coccinelle',
       author='Eric Leblond',
       author_email='eric@regit.org',
