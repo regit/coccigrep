d5bc4bf728ccec1e8c2389fe8bd977dc32bcfb81
Eric Leblond <eric@regit.org>
2011-09-14 09:28:12 +0200

    Prepare 1.1 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='coccigrep',
-      version='1.0',
+      version='1.1',
       description='Semantic grep for C based on coccinelle',
       author='Eric Leblond',
       author_email='eric@regit.org',
