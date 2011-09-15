878a1c220350461f1d6e917f0e3f5bdd9f7cccb7
Eric Leblond <eric@regit.org>
2011-09-15 19:23:03 +0200

    Prepare 1.2 version.
    This is a bugfix version which fixes a forgotten debug print.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='coccigrep',
-      version='1.1',
+      version='1.2',
       description='Semantic grep for C based on coccinelle',
       author='Eric Leblond',
       author_email='eric@regit.org',
