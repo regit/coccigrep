2479f593822ab5afd023707f4542e1459d98c5e6
Eric Leblond <eric@regit.org>
2020-05-04 13:56:20 +0200

    setup: add long description

--- setup.py
+++ setup.py
@@ -3,17 +3,23 @@ from setuptools import setup
 from src.coccigrep import COCCIGREP_VERSION
 
 import sys
+from os import path
 
 if (sys.version_info > (3, 0)):
     pygments_deps = 'pygments'
 else:
     pygments_deps = 'pygments<2.6.0'
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(name='coccigrep',
       version=COCCIGREP_VERSION,
       description='Semantic grep for C based on coccinelle',
       author='Eric Leblond',
       author_email='eric@regit.org',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',  # Optional (see note above)
       url='http://home.regit.org/software/coccigrep/',
       scripts=['coccigrep'],
       packages=['coccigrep'],
